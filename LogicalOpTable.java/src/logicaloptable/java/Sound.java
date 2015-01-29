82ce153c3bffb2575ec7d5bce369bfb8e962f579
Erin <Erin@Leo>
2015-01-29 18:35:01 -0500

    Try This 2-1

--- LogicalOpTable.java/src/logicaloptable/java/Sound.java
+++ LogicalOpTable.java/src/logicaloptable/java/Sound.java
@@ -18,8 +18,7 @@ public class Sound {
    public static void main(String args[] ) {
    double dist;
    
-   dist = 7.2 * 1100;
-   
+   dist = ( 14.4 / 2) * 1100;
    System.out.println("The lightning is " + dist + " feet away.");
    }
 }
