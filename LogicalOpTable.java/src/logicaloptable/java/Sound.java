51206438b648d3806cdc97b9b13dab62ab3b9edb
Erin <Erin@Leo>
2015-01-29 17:24:36 -0500

    Try This 2-1

---
+++ LogicalOpTable.java/src/logicaloptable/java/Sound.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logicaloptable.java;
+ /* 
+    Try This 2-1
+    Compute the distance to a lightning
+    strike whose sound takes 7.2 seconds
+    to reach you.
+    */
+/**
+ *
+ * @author Erin
+ */
+public class Sound {
+   public static void main(String args[] ) {
+   double dist;
+   
+   dist = 7.2 * 1100;
+   
+   System.out.println("The lightning is " + dist + " feet away.");
+   }
+}
